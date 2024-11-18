f71d93a51a96b65c283007ff64d17badcf3cf0a2
asss-whom <3059749821@qq.com>
2024-11-18 22:39:07 +0800

    Enhanced the readability of the program.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ use std::{
     thread, time,
 };
 
-static A_PRESS: AtomicBool = AtomicBool::new(false);
-static D_PRESS: AtomicBool = AtomicBool::new(false);
-static S_PRESS: AtomicBool = AtomicBool::new(false);
-static W_PRESS: AtomicBool = AtomicBool::new(false);
+static PRESSED: AtomicBool = AtomicBool::new(false);
 
 fn main() {
     if let Err(error) = listen(callback) {
@@ -28,52 +25,30 @@ fn send(event_type: &EventType) {
 }
 
 fn callback(event: Event) {
-    // Just a random magic number
+    // 300ms is close to the minimum interval allowed for two keyboard inputs in CS2.
     let delay = time::Duration::from_millis(300);
-    let last = time::Duration::from_millis(100);
+    let key_interval = time::Duration::from_millis(20);
 
-    if event.event_type == EventType::KeyRelease(rdev::Key::KeyA) {
-        if A_PRESS.load(Ordering::Relaxed) {
-            A_PRESS.store(false, Ordering::Relaxed);
-            return;
-        }
-        thread::sleep(delay);
-        send(&EventType::KeyPress(Key::KeyD));
-        D_PRESS.store(true, Ordering::Relaxed);
-        thread::sleep(last);
-        send(&EventType::KeyRelease(Key::KeyD));
+    if PRESSED.load(Ordering::Relaxed) {
+        PRESSED.store(false, Ordering::Relaxed);
+        return;
     }
-    if event.event_type == EventType::KeyRelease(rdev::Key::KeyD) {
-        if D_PRESS.load(Ordering::Relaxed) {
-            D_PRESS.store(false, Ordering::Relaxed);
-            return;
+
+    match event.event_type {
+        EventType::KeyRelease(key @ (Key::KeyA | Key::KeyD | Key::KeyS | Key::KeyW)) => {
+            let opposite = match key {
+                Key::KeyA => Key::KeyD,
+                Key::KeyD => Key::KeyA,
+                Key::KeyS => Key::KeyW,
+                Key::KeyW => Key::KeyS,
+                _ => unreachable!(),
+            };
+            thread::sleep(delay);
+            send(&EventType::KeyPress(opposite));
+            thread::sleep(key_interval);
+            PRESSED.store(true, Ordering::Relaxed);
+            send(&EventType::KeyRelease(opposite));
         }
-        thread::sleep(delay);
-        send(&EventType::KeyPress(Key::KeyA));
-        A_PRESS.store(true, Ordering::Relaxed);
-        thread::sleep(last);
-        send(&EventType::KeyRelease(Key::KeyA));
-    }
-    if event.event_type == EventType::KeyRelease(rdev::Key::KeyW) {
-        if W_PRESS.load(Ordering::Relaxed) {
-            W_PRESS.store(false, Ordering::Relaxed);
-            return;
-        }
-        thread::sleep(delay);
-        send(&EventType::KeyPress(Key::KeyS));
-        S_PRESS.store(true, Ordering::Relaxed);
-        thread::sleep(last);
-        send(&EventType::KeyRelease(Key::KeyS));
-    }
-    if event.event_type == EventType::KeyRelease(rdev::Key::KeyS) {
-        if S_PRESS.load(Ordering::Relaxed) {
-            S_PRESS.store(false, Ordering::Relaxed);
-            return;
-        }
-        thread::sleep(delay);
-        send(&EventType::KeyPress(Key::KeyW));
-        W_PRESS.store(true, Ordering::Relaxed);
-        thread::sleep(last);
-        send(&EventType::KeyRelease(Key::KeyW));
+        _ => (),
     }
 }
