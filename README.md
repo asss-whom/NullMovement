9c5c6479db51ab90a3083bfce0c161d5ca47fdc0
asss-whom <3059749821@qq.com>
2025-03-15 23:11:51 +0800

    Change to python version.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# NullMovement
+# Introduction to NullMovement 📚
 
-基于Rust `rdev` crate实现的一个急停宏。
+Welcome to the NullMovement! NullMovement is a sophisticated Counter-Strike 2 (CS2) counter-strafing program developed in Python. This library provides users with the ability to enhance their gameplay experience through precise movement management while ensuring compliance with the VAC (Valve Anti-Cheat) system. 🚀
+
+## Key Features 🌟
+
+NullMovement expertly utilizes timed delays to perform counter-strafes effectively, allowing players to maintain optimal speed and accuracy during engagements. The program's core functionality is designed to adapt to the dynamic nature of CS2, enabling seamless integration into your gameplay.
+
+## Customization Options ⚙️
+
+A standout feature of NullMovement is its user-configurable reaction time. By modifying the `REACTION_TIME` parameter, users can tailor the response speed to match their personal preferences and gameplay style. This adaptability ensures that players of all skill levels can benefit from the library.
+
+## Getting Started 🚀
+
+To utilize NullMovement, simply clone the repository and follow the installation instructions provided in the documentation. Once set up, you can easily adjust the `REACTION_TIME` to suit your needs. 
+
+```bash
+# Clone the repository
+git clone https://github.com/asss-whom/NullMovement.git
+
+# Navigate into the project directory
+cd NullMovement
+
+# Install uv (optional)
+pip intall uv
+
+# Install the necessary dependencies
+uv sync
+
+# or
+
+pip install -r requirements.txt
+
+```
+
+## Conclusion 🎮
+
+Thank you for exploring NullMovement; may your gameplay be ever more precise and enjoyable! 🎉
---
+++ main.py
@@ -0,0 +1,91 @@
+import logging
+import random
+import time
+
+import win32api
+import win32con
+from rich.logging import RichHandler
+
+FPS = 60  # frames
+REACTION_TIME = 0.2  # seconds
+
+
+def main():
+    log.info("✅ Program initialized successfully.")
+    log.info('⌨️ Press the "Esc" key to exit')
+
+    w = bool(win32api.GetAsyncKeyState(0x57) & 0x8000)
+    a = bool(win32api.GetAsyncKeyState(0x41) & 0x8000)
+    s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
+    d = bool(win32api.GetAsyncKeyState(0x44) & 0x8000)
+    while True:
+        w, a, s, d = event_loop(w, a, s, d)
+        if finish():
+            break
+
+    log.info("🎉 Program finished.")
+
+
+def finish() -> bool:
+    return bool(win32api.GetAsyncKeyState(0x1B) & 0x8000)  # Esc
+
+
+def event_loop(
+    last_w: bool,
+    last_a: bool,
+    last_s: bool,
+    last_d: bool,
+) -> tuple[bool, bool, bool, bool]:
+    w = bool(win32api.GetAsyncKeyState(0x57) & 0x8000)
+    a = bool(win32api.GetAsyncKeyState(0x41) & 0x8000)
+    s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
+    d = bool(win32api.GetAsyncKeyState(0x44) & 0x8000)
+
+    duration = random.normalvariate(REACTION_TIME, 0.05)
+
+    if not w and last_w:
+        log.debug('🔼 The "W" key has been released.')
+        time.sleep(duration)
+        win32api.keybd_event(0x53, 0, 0, 0)
+        time.sleep(0.03)
+        win32api.keybd_event(0x53, 0, win32con.KEYEVENTF_KEYUP, 0)
+        log.debug(f'🔽 The "S" key has been pressed ({duration * 1000:.2f}ms).')
+    if not a and last_a:
+        log.debug('🔼 The "A" key has been released.')
+        time.sleep(duration)
+        win32api.keybd_event(0x44, 0, 0, 0)
+        time.sleep(0.03)
+        win32api.keybd_event(0x44, 0, win32con.KEYEVENTF_KEYUP, 0)
+        log.debug(f'🔽 The "D" key has been pressed ({duration * 1000:.2f}ms).')
+    if not s and last_s:
+        log.debug('🔼 The "S" key has been released.')
+        time.sleep(duration)
+        win32api.keybd_event(0x57, 0, 0, 0)
+        time.sleep(0.03)
+        win32api.keybd_event(0x57, 0, win32con.KEYEVENTF_KEYUP, 0)
+        log.debug(f'🔽 The "W" key has been pressed ({duration * 1000:.2f}ms).')
+    if not d and last_d:
+        log.debug('🔼 The "D" key has been released.')
+        time.sleep(duration)
+        win32api.keybd_event(0x41, 0, 0, 0)
+        time.sleep(0.03)
+        win32api.keybd_event(0x41, 0, win32con.KEYEVENTF_KEYUP, 0)
+        log.debug(f'🔽 The "A" key has been pressed ({duration * 1000:.2f}ms).')
+    time.sleep(1 / FPS)
+    return w, a, s, d
+
+
+if __name__ == "__main__":
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+        level="NOTSET",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True, tracebacks_show_locals=True)],
+    )
+    log = logging.getLogger("rich")
+
+    try:
+        main()
+    except Exception as e:
+        log.exception(e)
