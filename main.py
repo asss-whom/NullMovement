c927ea69d1aecc20898cad57e80b94b758cfb9b8
asss-whom <3059749821@qq.com>
2025-03-16 13:57:43 +0800

    More stable.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import win32con
 from rich.logging import RichHandler
 
 FPS = 60  # frames
-REACTION_TIME = 0.2  # seconds
+REACTION_TIME = 0.1  # seconds
 
 
 def main():
@@ -41,34 +41,34 @@ def event_loop(
     s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
     d = bool(win32api.GetAsyncKeyState(0x44) & 0x8000)
 
-    duration = random.normalvariate(REACTION_TIME, 0.05)
+    duration = REACTION_TIME + random.uniform(-0.05, 0.05)
 
     if not w and last_w:
         log.debug('🔼 The "W" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x53, 0, 0, 0)
-        time.sleep(0.03)
+        time.sleep(0.05)
         win32api.keybd_event(0x53, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "S" key has been pressed ({duration * 1000:.2f}ms).')
     if not a and last_a:
         log.debug('🔼 The "A" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x44, 0, 0, 0)
-        time.sleep(0.03)
+        time.sleep(0.05)
         win32api.keybd_event(0x44, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "D" key has been pressed ({duration * 1000:.2f}ms).')
     if not s and last_s:
         log.debug('🔼 The "S" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x57, 0, 0, 0)
-        time.sleep(0.03)
+        time.sleep(0.05)
         win32api.keybd_event(0x57, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "W" key has been pressed ({duration * 1000:.2f}ms).')
     if not d and last_d:
         log.debug('🔼 The "D" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x41, 0, 0, 0)
-        time.sleep(0.03)
+        time.sleep(0.05)
         win32api.keybd_event(0x41, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "A" key has been pressed ({duration * 1000:.2f}ms).')
     time.sleep(1 / FPS)
