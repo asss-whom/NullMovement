51325b461b8698e6b8eb09a2f57d7c844cdccb40
asss-whom <3059749821@qq.com>
2025-03-20 18:40:14 +0800

    Revert "Enhanced readability."
    This reverts commit 509931c2cd94d485bd0ce85a1386293a31db4440.

--- main.py
+++ main.py
@@ -10,30 +10,18 @@ FPS = 60  # frames
 REACTION_TIME = 0.1  # seconds
 
 
-class State:
-    def __init__(self) -> None:
-        self.w = bool(win32api.GetAsyncKeyState(0x57) & 0x8000)
-        self.a = bool(win32api.GetAsyncKeyState(0x41) & 0x8000)
-        self.s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
-        self.d = bool(win32api.GetAsyncKeyState(0x44) & 0x8000)
-
-    def update(self, w: bool, a: bool, s: bool, d: bool) -> None:
-        self.w = w
-        self.a = a
-        self.s = s
-        self.d = d
-
-
-def main() -> None:
+def main():
     log.info("✅ Program initialized successfully.")
     log.info('🛑 Press the "End" key to exit')
 
-    state = State()
+    w = bool(win32api.GetAsyncKeyState(0x57) & 0x8000)
+    a = bool(win32api.GetAsyncKeyState(0x41) & 0x8000)
+    s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
+    d = bool(win32api.GetAsyncKeyState(0x44) & 0x8000)
     while True:
-        counter_strafe(state)
+        w, a, s, d = event_loop(w, a, s, d)
         if finish():
             break
-        time.sleep(1 / FPS)
 
     log.info("🎉 Program finished.")
 
@@ -42,7 +30,12 @@ def finish() -> bool:
     return bool(win32api.GetAsyncKeyState(0x23) & 0x8000)  # End
 
 
-def counter_strafe(last_state: State) -> None:
+def event_loop(
+    last_w: bool,
+    last_a: bool,
+    last_s: bool,
+    last_d: bool,
+) -> tuple[bool, bool, bool, bool]:
     w = bool(win32api.GetAsyncKeyState(0x57) & 0x8000)
     a = bool(win32api.GetAsyncKeyState(0x41) & 0x8000)
     s = bool(win32api.GetAsyncKeyState(0x53) & 0x8000)
@@ -50,35 +43,36 @@ def counter_strafe(last_state: State) -> None:
 
     duration = REACTION_TIME + random.uniform(-0.05, 0.05)
 
-    if not w and last_state.w:
+    if not w and last_w:
         log.debug('🔼 The "W" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x53, 0, 0, 0)
         time.sleep(0.05)
         win32api.keybd_event(0x53, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "S" key has been pressed ({duration * 1000:.2f}ms).')
-    if not a and last_state.a:
+    if not a and last_a:
         log.debug('🔼 The "A" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x44, 0, 0, 0)
         time.sleep(0.05)
         win32api.keybd_event(0x44, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "D" key has been pressed ({duration * 1000:.2f}ms).')
-    if not s and last_state.s:
+    if not s and last_s:
         log.debug('🔼 The "S" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x57, 0, 0, 0)
         time.sleep(0.05)
         win32api.keybd_event(0x57, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "W" key has been pressed ({duration * 1000:.2f}ms).')
-    if not d and last_state.d:
+    if not d and last_d:
         log.debug('🔼 The "D" key has been released.')
         time.sleep(duration)
         win32api.keybd_event(0x41, 0, 0, 0)
         time.sleep(0.05)
         win32api.keybd_event(0x41, 0, win32con.KEYEVENTF_KEYUP, 0)
         log.debug(f'🔽 The "A" key has been pressed ({duration * 1000:.2f}ms).')
-    last_state.update(w, a, s, d)
+    time.sleep(1 / FPS)
+    return w, a, s, d
 
 
 if __name__ == "__main__":
